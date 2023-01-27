5df5b6a3730549a661b5b911912f572673db4d8f
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-01-27 15:33:40 +0100

    html per sezione 3

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 let firstRow = document.getElementById("primaSezione");
 let secondRow = document.getElementById("secondaSezione");
+
 const fetchGenerale = async function (sito) {
   try {
     let res = await fetch(sito);
@@ -50,3 +51,27 @@ const fetchGenerale = async function (sito) {
 window.onload = fetchGenerale(
   "https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica"
 );
+
+{
+  /* <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> */
+}
